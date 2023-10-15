062abc445c2494177176543dee0279097cff8b65
Lokisonl <Lokisonl@qq.com>
2023-10-15 09:11:25 +0800

    Add zh_cn (#1)
    Add Simple Chineses localization

--- scripts/mods/BetterFireGrenade/BetterFireGrenade_localization.lua
+++ scripts/mods/BetterFireGrenade/BetterFireGrenade_localization.lua
@@ -1,30 +1,39 @@
 return {
 	mod_title = {
 		en = "Zealot Fire Particle Swap",
+		["zh-cn"] = "狂信徒燃烧弹特效替换",
 	},
 	mod_description = {
 		en =
 		"Replaces the fire lingering particles of the fire grenade to other particles for fun and accessibility. Some of the options are more obvious (if its friendly or not) than the original, others are there just for fun.",
+		["zh-cn"] = "将狂信徒火焰手榴弹的挥之不去的火焰替换为其他粒子特效，以获得乐趣和可视效果。某些选项比原来的更明显（无论是否友好），某些选项只是为了好玩。",
 	},
 	mod_vfx_type = {
 		en = "Particle Type"
+		["zh-cn"] = "特效类型",
 	},
 	mod_vfx_type_description = {
 		en = "Choose the particle type to replace the fire lingering particles of the fire grenade. RELOAD REQUIRED!"
+		["zh-cn"] = "选择特效类型来替换火焰手榴弹的火焰特效。需要重新启动游戏！",
 	},
 	mod_option_vfx_filled_one = {
 		en = "Nurgle Goo"
+		["zh-cn"] = "[更微弱]冷却的纳垢呕吐物",
 	},
 	mod_option_vfx_filled_two = {
 		en = "Beast of Nurgle Slime"
+		["zh-cn"] = "[更微弱]沸腾的纳垢呕吐物",
 	},
 	mod_option_vfx_filled_three = {
 		en = "Renegade Flamer"
+		["zh-cn"] = "[更明显]血痂火焰兵-红色烈焰",
 	},
 	mod_option_vfx_filled_four = {
 		en = "Renegade Pox Flamer"
+		["zh-cn"] = "[更明显]渣滓剧毒火焰兵-绿色烈焰",
 	},
 	mod_option_vfx_filled_five = {
 		en = "Original"
+		["zh-cn"] = "默认",
 	},
 }
