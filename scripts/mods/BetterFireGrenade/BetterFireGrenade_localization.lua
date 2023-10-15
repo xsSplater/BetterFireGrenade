22dc792315b0f0d5301a83951fe700db9cff40c9
xsSplater <xssplater@gmail.com>
2023-10-15 04:19:14 +0300

    RU BetterFireGrenade localization (#2)
    Add ru localization;
    Fix commans;

--- scripts/mods/BetterFireGrenade/BetterFireGrenade_localization.lua
+++ scripts/mods/BetterFireGrenade/BetterFireGrenade_localization.lua
@@ -2,38 +2,47 @@ return {
 	mod_title = {
 		en = "Zealot Fire Particle Swap",
 		["zh-cn"] = "狂信徒燃烧弹特效替换",
+		ru = "Замена частиц огня гранат Изувера",
 	},
 	mod_description = {
 		en =
 		"Replaces the fire lingering particles of the fire grenade to other particles for fun and accessibility. Some of the options are more obvious (if its friendly or not) than the original, others are there just for fun.",
 		["zh-cn"] = "将狂信徒火焰手榴弹的挥之不去的火焰替换为其他粒子特效，以获得乐趣和可视效果。某些选项比原来的更明显（无论是否友好），某些选项只是为了好玩。",
+		ru = "Zealot Fire Particle Swap - Заменяет частицы огня огненной гранаты Изувера на другие частицы для развлечения и удобства. Некоторые варианты более различимы(дружественные они или нет), чем оригинал, другие добавлены просто для развлечения.",
 	},
 	mod_vfx_type = {
-		en = "Particle Type"
+		en = "Particle Type",
 		["zh-cn"] = "特效类型",
+		ru = "Тип частиц",
 	},
 	mod_vfx_type_description = {
-		en = "Choose the particle type to replace the fire lingering particles of the fire grenade. RELOAD REQUIRED!"
+		en = "Choose the particle type to replace the fire lingering particles of the fire grenade. RELOAD REQUIRED!",
 		["zh-cn"] = "选择特效类型来替换火焰手榴弹的火焰特效。需要重新启动游戏！",
+		ru = "Выберите тип частиц для замены частиц огня гранаты Изувера. ТРЕБУЕТСЯ ПЕРЕЗАГРУЗКА МОДОВ!",
 	},
 	mod_option_vfx_filled_one = {
-		en = "Nurgle Goo"
+		en = "Nurgle Goo",
 		["zh-cn"] = "[更微弱]冷却的纳垢呕吐物",
+		ru = "Слизь Нургла",
 	},
 	mod_option_vfx_filled_two = {
-		en = "Beast of Nurgle Slime"
+		en = "Beast of Nurgle Slime",
 		["zh-cn"] = "[更微弱]沸腾的纳垢呕吐物",
+		ru = "Бурлящая слизь зверя Нургла.",
 	},
 	mod_option_vfx_filled_three = {
-		en = "Renegade Flamer"
+		en = "Renegade Flamer",
 		["zh-cn"] = "[更明显]血痂火焰兵-红色烈焰",
+		ru = "Огонь Скаба-огнемётчика",
 	},
 	mod_option_vfx_filled_four = {
-		en = "Renegade Pox Flamer"
+		en = "Renegade Pox Flamer",
 		["zh-cn"] = "[更明显]渣滓剧毒火焰兵-绿色烈焰",
+		ru = "Огонь Дрега-токс-огнемётчика",
 	},
 	mod_option_vfx_filled_five = {
-		en = "Original"
+		en = "Original",
 		["zh-cn"] = "默认",
+		ru = "Обычный огонь",
 	},
 }
